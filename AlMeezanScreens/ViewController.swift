15f83b88ea995c835573c43cc7e603952c55fde3
Ahmed <mahmedrival@gmail.com>
2022-07-27 16:11:13 +0500

    Initial Commit

---
+++ AlMeezanScreens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlMeezanScreens
+//
+//  Created by Ahmad on 27/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
